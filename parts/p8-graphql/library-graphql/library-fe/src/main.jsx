a9e77ff9b4fc9d67514317868105e06a2ba45ff2
2underscores <jeremy__smith@hotmail.com>
2024-12-28 18:55:21 +1100

    Added auth header to all FE graphQL requests

--- parts/p8-graphql/library-graphql/library-fe/src/main.jsx
+++ parts/p8-graphql/library-graphql/library-fe/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 import { BrowserRouter } from "react-router";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context'
 import { createTheme, ThemeProvider } from '@mui/material';
 import { orange } from '@mui/material/colors';
 
@@ -19,8 +20,25 @@ const theme = createTheme({
   },
 });
 
-// Slow loading of the page
-const client = new ApolloClient({ uri: "http://localhost:4000", cache: new InMemoryCache() });
+// Apollo Client
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('userAuthTokenStr')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
